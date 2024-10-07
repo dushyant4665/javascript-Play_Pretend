b56f4829a9152fefcde2620103fd271ecafc94c3
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-10-07 22:27:25 +0530

    Update formerD.structure.js

--- formerD.structure.js
+++ formerD.structure.js
@@ -12,9 +12,9 @@ var findStudent =(allStudent,studentName)=>{
         if(allStudent[i]===studentName){
             console.log(`found ${studentName} at ${[i]}`)
         }
-        // else{
-        //     console.log('')
-        // }
+        else{
+            // console.log('')
+        }
 }}
 
 findStudent(allStudent,'aditya');
@@ -35,39 +35,3 @@ findStudent(allStudent,'aditya');
 
 
 
-
-// const allStudents=['aman','bharat','chetan','dushyant','eman','farukl','golu','hitesh']
-
-
-// const findStudent=(allStudents,studentName)=>{
-//     for(let i =0; i<allStudents.length; i++){
-//         if(allStudents[i]===studentName){
-//             console.log(`found ${studentName} at ${[i]}`)
-//         }
-//         // else{
-//         //     console.log('not found')
-//         // }
-//     }
-// }
-
-// findStudent(allStudents,'eman')
-
-
-
-
-
-// const allStudent=['hitesh','vikash','chavan','swara','manish','dipesh','rancho','ayush']
-
-// var findStudent=(allStudent,studentName)=>{
-         
-//        for(let i=0;i<allStudent.length;i++){
-//         if(allStudent[i]===studentName){
-//             console.log(`found ${studentName} at ${i}`)
-//         }
-//         else{
-//             // console.log('not found')
-//         }
-//     }
-// }
-
-// findStudent(allStudent,'rancho')
